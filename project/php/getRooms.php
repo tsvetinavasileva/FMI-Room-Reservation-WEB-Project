3d1529da937ca916121143713fbb279539596221
gdgenchev <gdgenchev97@gmail.com>
2020-01-30 17:03:45 +0200

    Remove files

--- project/php/getRooms.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "persistence/SQLPersistenceService.php";
-require_once "util/DbConnectionCreator.php";
-
-$conn = DbConnectionCreator::createConnection();
-$persistenceService = new SQLPersistenceService($conn);
-$rooms = $persistenceService->getRooms();
-echo json_encode($rooms);
-
--- project/php/persistence/PersistenceService.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-interface PersistenceService
-{
-    function getRooms();
-
-    function getBuildings();
-}
