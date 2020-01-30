4310ead4bbe87a6ca20447c270280d7ff33569bc
gdgenchev <gdgenchev97@gmail.com>
2020-01-30 23:50:41 +0200

    Add logic for getting resources from the server

--- project/js/register/register.js
+++ project/js/register/register.js
@@ -1,12 +1,27 @@
-var select = document.getElementById("building-select");
-
 $.ajax({
     url: "../../project/php/getBuildings.php",
     context: document.body,
     success: function(response) {
+        var select = document.getElementById("building-select");
         let buildingsNames = JSON.parse(response);
         for (let i = 0; i < buildingsNames.length; i++) {
             select.options[select.options.length] = new Option(buildingsNames[i]);
+        }
+    }
+});
+
+$.ajax({
+    url: "../../project/php/getResources.php",
+    context: document.body,
+    success: function(response) {
+        var select = document.getElementById("resources-select");
+        let resources = JSON.parse(response);
+        select.setAttribute("size", resources.length)
+        for (let i = 0; i < resources.length; i++) {
+            const option = new Option();
+            option.innerHTML = resources[i]["iconCode"] + " " + resources[i]["resourceName"];
+            option.setAttribute("value", resources[i]["resourceName"]);
+            select.options[select.options.length] = option;
         }
     }
 });
